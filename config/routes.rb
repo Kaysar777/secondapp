7c34d16c6a892eea646952d3556c758ef45781ed
Kaisar Butin <bolawak@gmail.com>
2014-05-26 17:55:29 +0600

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Secondapp::Application.routes.draw do
-  get "users/new"
+  resources :users
 
   root 'static_pages#home'
   
---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Bla Fra"
+    email    "srfrwf@erierogij.te"
+    password "wedwed"
+    password_confirmation "wedwed"
+  end
+end
