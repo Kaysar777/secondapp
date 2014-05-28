33bef4fc45fa9ad21533e055eba49ff3c5770c41
Kaisar Butin <bolawak@gmail.com>
2014-05-28 15:10:39 +0600

    Finish user edit, update, index, and destroy actions

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,12 @@
 FactoryGirl.define do
   factory :user do
-    name     "Bla Fra"
-    email    "srfrwf@erierogij.te"
-    password "wedwed"
-    password_confirmation "wedwed"
+    sequence(:name)  { |n| "Person #{n}" }
+    sequence(:email) { |n| "person_#{n}@example.com"}
+    password "foobar"
+    password_confirmation "foobar"
+
+    factory :admin do
+      admin true
+    end
   end
 end
